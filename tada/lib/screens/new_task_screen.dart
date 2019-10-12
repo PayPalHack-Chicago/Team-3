ca57c5f68efa7ac59b92a46004ef89718fcd4c71
mathias kniesel <yuriyadaskevych35@gmail.com>
2019-10-12 06:30:13 -0700

    completed task page and profile page

---
+++ tada/lib/screens/new_task_screen.dart
@@ -0,0 +1,338 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_cupertino_date_picker/flutter_cupertino_date_picker.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    DateTimePickerTheme pickerTheme = DateTimePickerTheme.Default;
+
+
+    double heightSize = MediaQuery.of(context).size.height;
+    double widthSize = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('Create Task Page'),
+      ),
+      body: SingleChildScrollView(
+        child: Stack(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(bottom: heightSize * 0.04),
+                ),
+                Container(
+                  //Enter Title
+                  padding: EdgeInsets.only(bottom: 20),
+                  width: widthSize * 0.90,
+                  height: heightSize * 0.10,
+                  child: TextField(
+                    obscureText: false,
+                    onChanged: (text) {
+                      print('sup loser');
+                    },
+                    decoration: new InputDecoration(
+                      filled: true,
+                      fillColor: Color(0x60eaeaea),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(5.0),
+                        ),
+                        borderSide: BorderSide(
+                          color: Colors.blue,
+                        ),
+                      ),
+                      enabledBorder: const OutlineInputBorder(
+                        borderSide:
+                        const BorderSide(color: Colors.blue, width: 0.3),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(5.0),
+                        ),
+                      ),
+                      labelText: 'Enter Title',
+                      labelStyle: TextStyle(
+                        color: Colors.black.withOpacity(0.4),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  //Enter Description
+                  padding: EdgeInsets.only(bottom: 20),
+                  width: widthSize * 0.90,
+                  height: heightSize * 0.10,
+                  child: TextField(
+                    keyboardType: TextInputType.multiline,
+                    maxLines: null,
+                    obscureText: false,
+                    onChanged: (text) {
+                      print('sup loser');
+                    },
+                    decoration: new InputDecoration(
+                      filled: true,
+                      fillColor: Color(0x60eaeaea),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(5.0),
+                        ),
+                        borderSide: BorderSide(
+                          color: Colors.blue,
+                        ),
+                      ),
+                      enabledBorder: const OutlineInputBorder(
+                        borderSide:
+                        const BorderSide(color: Colors.blue, width: 0.3),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(5.0),
+                        ),
+                      ),
+                      labelText: 'Enter Description',
+                      labelStyle: TextStyle(
+                        color: Colors.black.withOpacity(0.4),
+                      ),
+                    ),
+                  ),
+                ),
+                DatePickerWidget(
+                  pickerTheme: DateTimePickerTheme(
+                      showTitle: false
+                  ),
+                ),
+                Container(
+                  //Location of Task
+                  padding: EdgeInsets.only(bottom: 20),
+                  width: widthSize * 0.90,
+                  height: heightSize * 0.07,
+                  child: TextField(
+                    obscureText: false,
+                    onChanged: (text) {
+                      print('sup loser');
+                    },
+                    decoration: new InputDecoration(
+                      filled: true,
+                      fillColor: Color(0x60eaeaea),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(5.0),
+                        ),
+                        borderSide: BorderSide(
+                          color: Colors.blue,
+                        ),
+                      ),
+                      enabledBorder: const OutlineInputBorder(
+                        borderSide:
+                        const BorderSide(color: Colors.blue, width: 0.3),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(5.0),
+                        ),
+                      ),
+                      labelText: 'Location of Task',
+                      labelStyle: TextStyle(
+                        color: Colors.black.withOpacity(0.4),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  //Requirements for Task
+                  padding: EdgeInsets.only(bottom: 20),
+                  width: widthSize * 0.90,
+                  child: TextField(
+                    keyboardType: TextInputType.multiline,
+                    maxLines: null,
+                    obscureText: false,
+                    onChanged: (text) {
+                      print('sup loser');
+                    },
+                    decoration: new InputDecoration(
+                      filled: true,
+                      fillColor: Color(0x60eaeaea),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(5.0),
+                        ),
+                        borderSide: BorderSide(
+                          color: Colors.blue,
+                        ),
+                      ),
+                      enabledBorder: const OutlineInputBorder(
+                        borderSide:
+                        const BorderSide(color: Colors.blue, width: 0.3),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(5.0),
+                        ),
+                      ),
+                      labelText: 'Requirements for Task',
+                      labelStyle: TextStyle(
+                        color: Colors.black.withOpacity(0.4),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  //Enter Contact Info
+                  padding: EdgeInsets.only(bottom: 20),
+                  width: widthSize * 0.90,
+                  height: heightSize * 0.07,
+                  child: TextField(
+                    keyboardType: TextInputType.multiline,
+                    maxLines: null,
+                    obscureText: false,
+                    onChanged: (text) {
+                      print('sup loser');
+                    },
+                    decoration: new InputDecoration(
+                      filled: true,
+                      fillColor: Color(0x60eaeaea),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(5.0),
+                        ),
+                        borderSide: BorderSide(
+                          color: Colors.blue,
+                        ),
+                      ),
+                      enabledBorder: const OutlineInputBorder(
+                        borderSide:
+                        const BorderSide(color: Colors.blue, width: 0.3),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(5.0),
+                        ),
+                      ),
+                      labelText: 'Enter Contact Info',
+                      labelStyle: TextStyle(
+                        color: Colors.black.withOpacity(0.4),
+                      ),
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    //First Name text input
+                    Container(
+                      height: heightSize * 0.04,
+                      width: widthSize * 0.32,
+                      child: TextField(
+                        obscureText: false,
+                        onChanged: (text) {},
+                        decoration: new InputDecoration(
+                          filled: true,
+                          fillColor: Color(0x60eaeaea),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(5.0),
+                            ),
+                            borderSide: BorderSide(
+                              color: Colors.blue,
+                            ),
+                          ),
+                          enabledBorder: const OutlineInputBorder(
+                            borderSide: const BorderSide(
+                                color: Colors.blue, width: 0.3),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(5.0),
+                            ),
+                          ),
+                          labelText: 'Points',
+                          labelStyle: TextStyle(
+                            color: Colors.black.withOpacity(0.4),
+                          ),
+                        ),
+                      ),
+                    ),
+                    //Last Name text input
+                    Container(
+                      child: Text('or'),
+                    ),
+                    Container(
+                      height: heightSize * 0.04,
+                      width: widthSize * 0.32,
+                      child: TextField(
+                        obscureText: false,
+                        onChanged: (text) {},
+                        decoration: new InputDecoration(
+                          filled: true,
+                          fillColor: Color(0x60eaeaea),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(5.0),
+                            ),
+                            borderSide: BorderSide(
+                              color: Colors.blue,
+                            ),
+                          ),
+                          enabledBorder: const OutlineInputBorder(
+                            borderSide: const BorderSide(
+                                color: Colors.blue, width: 0.3),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(5.0),
+                            ),
+                          ),
+                          labelText: 'Money',
+                          labelStyle: TextStyle(
+                            color: Colors.black.withOpacity(0.4),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  padding: EdgeInsets.only(bottom: 20),
+                  width: widthSize * 0.90,
+                ),
+                Card(
+                  elevation: 2,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: InkWell(
+                    onTap: () {},
+                    child: Container(
+                      width: widthSize * 0.35,
+                      height: heightSize * 0.05,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black12, width: 2.0),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Submit Task",
+                        style: TextStyle(
+                          fontSize: 15.0,
+                          color: Colors.blue,
+                        ), //fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ tada/lib/screens/profile_org_screen.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ProfilePage(),
+    );
+  }
+}
+
+class ProfilePage extends StatefulWidget {
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    double heightSize = MediaQuery.of(context).size.height;
+    double widthSize = MediaQuery.of(context).size.width;
+    return MaterialApp(
+      title: 'Org Bio',
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.only(bottom: heightSize * 0.04),
+              child: Image.asset('images/ESI.png'),
+            ),
+            Container(
+              child: Text(
+                'ESI - Exelon Summer Institute',
+                style: TextStyle(fontSize: 30.0),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(bottom: heightSize * 0.09),
+              child: Text(
+                'esiemail@iit.edu',
+                style: TextStyle(fontSize: 20.0),
+              ),
+            ),
+            Container(
+              child: Card(
+                elevation: 1,
+                child: Text(
+                  'As a world-class institution of higher learning IIT recognizes the critical importance'
+                      'of the universitys mission to advance knowledge through research and scholarship to culivate invention'
+                      ' imporoving the human condition and to educate students from throughout the world for a life of professional'
+                      ' achievement service to society and individual fulfillment moving our world toward a more sustainable path',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
